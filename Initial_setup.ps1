6d147b45b8479bd4f90c220bcedbe1389b7d8fb2
Tumendelger Erdenebayar <tumendelger.e@adelphi.digital>
2019-02-15 01:25:32 +1100

    Customize PS with ConEmu and other modules

--- Initial_setup.ps1
+++ Initial_setup.ps1
@@ -7,3 +7,19 @@ mkdir $PROFILE_DIR -ErrorAction SilentlyContinue
 
 # Copy profile script to new location
 Copy-Item .\Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\Documents\WindowPowerShell
+
+# Install Chocolatey packages
+choco install git.install -y
+choco install conemu -y
+choco install openssh -y
+
+# Install Nuget for PS
+Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+# Install PowerShell modules
+# Module to colorize directory listing
+Install-Module Get-ChildItemColor
+
+# Module to be used to show git info on terminal
+Install-Module -Name 'posh-git'
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,12 @@
+# Import module posh-git
+Import-Module -Name posh-git
+
+#region Environment variables
+$env:Path = "$env:Path;C:\Program Files\OpenSSH-Win64"
+$Env:USERNAME = "Tumendelger Erdenebayar"
+#endregion
+
+#region Aliases
 function goto_github_folder {Set-location $env:USERPROFILE\Sources\github}
 function goto_gitlab_folder {Set-location $env:USERPROFILE\Sources\gitlab}
 function goto_bitbucket_folder {Set-location $env:USERPROFILE\Sources\bitbucket}
@@ -5,3 +14,29 @@ function goto_bitbucket_folder {Set-location $env:USERPROFILE\Sources\bitbucket}
 Set-Alias github goto_github_folder
 Set-Alias gitlab goto_gitlab_folder
 Set-Alias bitbucket goto_bitbucket_folder
+
+Set-Alias ls Get-ChildItemColor -option AllScope -Force
+Set-Alias dir Get-ChildItemColor -option AllScope -Force
+#endregion
+
+#region Code
+function prompt {
+  $realLASTEXITCODE = $LASTEXITCODE
+
+  Write-Host "$ENV:USERNAME @ " -NoNewline -ForegroundColor DarkYellow
+  Write-Host "$ENV:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
+  Write-Host " : " -NoNewline -ForegroundColor DarkGray
+  Write-Host $($(Get-Location) -replace ($env:USERPROFILE).Replace('\', '\\'), "~") -NoNewline -ForegroundColor Blue
+  Write-Host " : " -NoNewline -ForegroundColor DarkGray
+  Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor DarkMagenta
+  Write-Host " : " -NoNewline -ForegroundColor DarkGray
+
+  $global:LASTEXITCODE = $realLASTEXITCODE
+
+  Write-VcsStatus
+
+  Write-Host
+
+  return "> "
+}
+#endregion
